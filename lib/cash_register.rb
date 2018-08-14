5a4c78ea76351a6fae4533c1ebba62a312cf8622
jamieberrier <berrier.jamie@gmail.com>
2018-08-14 01:59:02 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ require 'pry'
 class CashRegister
   attr_accessor :total
 
-    def initialize(employee_discount=discount)
+    def initialize(employee_discount=0)
 
     @total = 0
     @employee_discount = employee_discount
